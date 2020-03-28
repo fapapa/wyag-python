5b046f65381f59dc317b8969c620dd587fdba4e3
Fabio Papa <fabtheman@gmail.com>
2020-03-28 11:47:58 -0700

    Define abstract GitObject for sub-classing later
    GitObject is an abstract class that must be sub-classed. The four types
    of objects in Git—and therefore four sub-classes that we will eventually
    need are: `blob`, `commit`, `tag`, and `tree`.

--- libwyag.py
+++ libwyag.py
@@ -165,3 +165,27 @@ def repo_find(path=".", required=True):
 
     # Recursive case
     return repo_find(parent, required)
+
+class GitObject(object):
+
+    repo = None
+
+    def __init__(self, repo, data=None):
+        self.repo = repo
+
+        if data != None:
+            self.deserialize(data)
+
+    def serialize(self):
+        """This function must be implemented by subclasses.
+
+It must read the object's contents from self.data, a byte string, and do
+whatever it takes to convert it into a meaningful representation. What exactly
+that means depends on each subclass.
+
+        """
+
+        raise Exception("Unimplemented!")
+
+    def deserialize(self, data):
+        raise Exception("Unimplemented!")
