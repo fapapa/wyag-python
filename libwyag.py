174a70fe32360571cb45b36187a609d3e6880b2f
Fabio Papa <fabtheman@gmail.com>
2020-03-29 09:35:05 -0700

    Add support for tag objects
    A tag has the same format as a commit, just the name of it changes.

--- libwyag.py
+++ libwyag.py
@@ -587,3 +587,6 @@ def show_ref(repo, refs, with_hash=True, prefix=""):
                      v,
                      with_hash=with_hash,
                      prefix="{0}{1}{2}".format(prefix, "/" if prefix else "", k))
+
+class GitTag(GitCommit):
+    fmt=b'tag'
