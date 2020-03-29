7d738e731f9eafe1f8b2dffd49ff506835251f15
Fabio Papa <fabtheman@gmail.com>
2020-03-29 14:39:37 -0700

    Add a GitIndexEntry class to handle staging

--- libwyag.py
+++ libwyag.py
@@ -709,3 +709,42 @@ def cmd_rev_parse(args):
     repo = repo_find()
 
     print(object_find(repo, args.name, args.type, follow=True))
+
+class GitIndexEntry(object):
+    ctime=None
+    """The last time a file's metadata changed. This is a tuple (seconds, nanoseconds)"""
+
+    mtime=None
+    """The last time a file's data changed. This is a tuple (seconds, nanoseconds)"""
+
+    dev=None
+    """The ID of device containing this file"""
+
+    ino=None
+    """The file's inode number"""
+
+    mode_type=None
+    """The object type, either b1000 (regular), b1010 (symlink), b1110 (gitlink)."""
+
+    mode_perms=None
+    """"The object permisions, an integer."""
+
+    uid=None
+    """User ID of owner"""
+
+    gid=None
+    """Group ID of owner (according to stat 2)"""
+
+    size=None
+    """Size of this object, in bytes"""
+
+    obj=None
+    """The object's hash and hex string"""
+
+    flag_assume_valid=None
+    flag_extended=None
+    flag_stage=None
+    flag_name_length=None
+    """Length of the name if < 0xFFF (yes, three F's), -1 otherwise"""
+
+    name=None
